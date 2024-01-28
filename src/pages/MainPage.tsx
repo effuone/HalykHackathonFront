25cb8dad8c4780023a2f94ac30307add0ce67385
zhqriks <zhorikmartanov228@gmail.com>
2024-01-28 09:27:52 +0600

    fix title main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,7 +3,7 @@ import ApplicationForm from "@/components/ApplicationForm/ApplicationForm.tsx";
 
 const MainPage: React.FC = () => {
   return (
-    <MainLayout headerText={'Продукты'}>
+    <MainLayout headerText={'Вероятность Исполнения Страхового Полиса'}>
       <ApplicationForm />
     </MainLayout>
   );
