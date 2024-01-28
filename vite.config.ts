c055145e86f780f18de948d954809a26f3747608
zhqriks <zhorikmartanov228@gmail.com>
2024-01-28 09:46:03 +0600

    add polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import * as path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
 });
+
